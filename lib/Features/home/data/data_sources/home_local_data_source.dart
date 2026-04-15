864ac9396dd471756a402d8d57df141e82ffabec
Ahmed Ismail <ah.hany5050@gmail.com>
2026-04-15 17:42:58 +0200

    feat: implement local data retrieval for featured and newest books using Hive

--- lib/Features/home/data/data_sources/home_local_data_source.dart
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -1,4 +1,6 @@
 import 'package:clean_arch_bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:clean_arch_bookly_app/constants.dart';
+import 'package:hive/hive.dart';
 
 abstract class HomeLocalDataSource {
   // ال type هنا هيكون list<BookEntity> من غير Future عشان دي بيانات محليه يعني مش بتاخد وقت في جلبها زي ال remote data source اللي بتاخد وقت في جلب البيانات من الانترنت فبنستخدم Future عشان نقدر نتعامل مع الوقت ده اما هنا فبنرجع البيانات مباشره من غير ما نحتاج نتعامل مع الوقت لانها متكيشة عندي في الجهاز
@@ -12,11 +14,14 @@ class HomeLocalDataSourceImpl implements HomeLocalDataSource {
   List<BookEntity> getLastFeaturedBooks() {
     // هنا ممكن نستخدم اي طريقة لتخزين البيانات محليا زي shared preferences او hive او sqflite او اي طريقة تانية حسب ما يناسب التطبيق بتاعك
     // في المثال ده انا هستخدم hive لتخزين البيانات اللي جايالي من ال api محليا
-    return [];
+    var box = Hive.box<BookEntity>(kFeaturedBox);
+    return box.values.toList();
   }
 
   @override
   List<BookEntity> getLastNewestBooks() {
-    return [];
+    // هنا انا بجيب البيانات اللي مخزنة عندي في ال hive box اللي اسمه kNewestBox وبحولها لقائمة من ال BookEntity وبيرجعها لي ال homeRepo اللي بيستخدمها في ال use case بتاعه عشان يقدر يعرضها في ال ui بتاعه
+    var box = Hive.box<BookEntity>(kNewestBox);
+    return box.values.toList();
   }
 }
