58c4dbab7c1f80c1c0fad0f6e89b955bffee8458
Ahmed Ismail <ah.hany5050@gmail.com>
2026-04-15 16:35:17 +0200

    feat: create home local data source

---
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -0,0 +1,22 @@
+import 'package:clean_arch_bookly_app/Features/home/domain/entities/book_entity.dart';
+
+abstract class HomeLocalDataSource {
+  // ال type هنا هيكون list<BookEntity> من غير Future عشان دي بيانات محليه يعني مش بتاخد وقت في جلبها زي ال remote data source اللي بتاخد وقت في جلب البيانات من الانترنت فبنستخدم Future عشان نقدر نتعامل مع الوقت ده اما هنا فبنرجع البيانات مباشره من غير ما نحتاج نتعامل مع الوقت لانها متكيشة عندي في الجهاز
+  // انا سميت هنا getLastFeaturedBooks و getLastNewestBooks عشان دي بتجيب اخر بيانات جتلي من ال api يعني اخر قائمة كتب جتلي من ال api سواء كانت featured او newest ودي بتكون مخزنة عندي في الجهاز فلو حصل اي مشكلة في الانترنت او اي حاجة تانية اقدر اجيب البيانات اللي عندي في الجهاز بدل ما اجيبها من ال api مرة تانية ودي بتكون مفيدة جدا في حالة ان المستخدم مش متصل بالانترنت او حصل اي مشكلة في الاتصال بالانترنت فبيقدر يشوف البيانات اللي عنده في الجهاز بدل ما يشوف رسالة خطأ او حاجة زي كده
+  List<BookEntity> getLastFeaturedBooks();
+  List<BookEntity> getLastNewestBooks();
+}
+
+class HomeLocalDataSourceImpl implements HomeLocalDataSource {
+  @override
+  List<BookEntity> getLastFeaturedBooks() {
+    // هنا ممكن نستخدم اي طريقة لتخزين البيانات محليا زي shared preferences او hive او sqflite او اي طريقة تانية حسب ما يناسب التطبيق بتاعك
+    // في المثال ده انا هستخدم hive لتخزين البيانات اللي جايالي من ال api محليا
+    return [];
+  }
+
+  @override
+  List<BookEntity> getLastNewestBooks() {
+    return [];
+  }
+}
