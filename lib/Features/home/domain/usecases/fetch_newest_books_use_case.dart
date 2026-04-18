ba0963450782ad9668d931f643c31f65b46510a0
Ahmed Ismail <ah.hany5050@gmail.com>
2026-04-18 17:31:21 +0200

    refactor: update FetchBestNewestBooksUseCase to accept page number parameter

--- lib/Features/home/domain/usecases/fetch_newest_books_use_case.dart
+++ lib/Features/home/domain/usecases/fetch_newest_books_use_case.dart
@@ -1,16 +1,16 @@
 import 'package:clean_arch_bookly_app/Core/error/failures.dart';
-import 'package:clean_arch_bookly_app/Core/use_cases/no_parameter_use_case.dart';
+import 'package:clean_arch_bookly_app/Core/use_cases/use_case.dart';
 import 'package:clean_arch_bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:clean_arch_bookly_app/Features/home/domain/repositories/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchBestNewestBooksUseCase implements UseCase<List<BookEntity>> {
+class FetchBestNewestBooksUseCase implements UseCase<List<BookEntity>, int> {
   final HomeRepo homeRepo;
 
   FetchBestNewestBooksUseCase(this.homeRepo);
 
   @override
-  Future<Either<Failure, List<BookEntity>>> call([int param =0]) async {
+  Future<Either<Failure, List<BookEntity>>> call([int param = 0]) async {
     return await homeRepo.getNewestBooks(pageNumber: param);
   }
 }
